8f8c1b777ad8e13879aac711e2c019f3bf0e6557
Jake Rasmusson <jakerasmusson@gmail.com>
2024-07-07 19:37:12 -0500

    routes

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
-mongoose.connect('mongodb://127.0.0.1:27017/socialNetworkDb')
+mongoose.connect(process.env.Mongo_DB_URL || 'mongodb://127.0.0.1:27017/socialNetworkDb')
 
 module.exports = mongoose.connection
--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const routes = require('./controllers')
 const db = require('./config/connection')
 const app = express()
 const PORT = process.env.PORT || 3001
+require('dotenv').config()
 
+app.use(express.json())
 app.use(routes)
 
 db.once('open', () => {
